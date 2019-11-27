992bfbe9cc5b60f089cd9c42b3edff51c5436baf
Eduardo Gallego garcia <egallego@c3r8s1.42madrid.com>
2019-11-27 20:08:04 +0100

    progress

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,23 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/20 21:36:20 by egallego          #+#    #+#             */
-/*   Updated: 2019/11/27 19:31:35 by egallego         ###   ########.fr       */
+/*   Created: 2019/11/20 18:10:32 by egallego          #+#    #+#             */
+/*   Updated: 2019/11/26 17:22:32 by egallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <string.h>
-
-void        *ft_memset(void *b, int c, size_t len);
-void 		ft_bzero(void *s, size_t n);
-void		*ft_calloc(size_t count, size_t size);
-void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-
-#endif
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/20 21:36:20 by egallego          #+#    #+#             */
-/*   Updated: 2019/11/27 19:31:35 by egallego         ###   ########.fr       */
+/*   Created: 2019/11/27 19:30:26 by egallego          #+#    #+#             */
+/*   Updated: 2019/11/27 20:08:04 by egallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <string.h>
+void		*memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	
 
-void        *ft_memset(void *b, int c, size_t len);
-void 		ft_bzero(void *s, size_t n);
-void		*ft_calloc(size_t count, size_t size);
-void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+}
 
-#endif
