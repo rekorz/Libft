9143a137255a0d2deb66cccc6bfbf5b766e6fed3
Eduardo Gallego garcia <egallego@c3r6s3.42madrid.com>
2019-12-03 17:43:42 +0100

    v.1 ft_memccpy.c done with error correction

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/20 21:36:20 by egallego          #+#    #+#             */
-/*   Updated: 2019/12/03 16:32:53 by egallego         ###   ########.fr       */
+/*   Created: 2019/12/03 15:22:01 by egallego          #+#    #+#             */
+/*   Updated: 2019/12/03 16:52:43 by egallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
+#include "libft.h"
+#include <stdio.h>
 
-void        *ft_memset(void *b, int c, size_t len);
-void 		ft_bzero(void *s, size_t n);
-void		*ft_calloc(size_t count, size_t size);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t				i;
+	unsigned char		*source;
+	unsigned char		*destiny;
 
-#endif
+	i = -1;
+	source = (unsigned char *)src;
+	destiny = (unsigned char *)dst;
+	while (++i < n)
+	{
+		destiny[i] = source[i];
+		if (source[i] == (unsigned char)c && source[i])
+			return (&destiny[i + 1]);
+	}
+	return (0);
+}
