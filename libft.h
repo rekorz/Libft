bd2db0c6c11b2035f5ff5671375d9619bee40e5f
Eduardo Gallego garcia <egallego@c3r6s5.42madrid.com>
2019-12-08 17:15:41 +0100

    v.1.0 ft_strlen.c norme:ok t1:check t2:check

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 21:36:20 by egallego          #+#    #+#             */
-/*   Updated: 2019/12/08 13:29:19 by egallego         ###   ########.fr       */
+/*   Updated: 2019/12/08 17:07:27 by egallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t		ft_strlen(const char *s);
 
 #endif
