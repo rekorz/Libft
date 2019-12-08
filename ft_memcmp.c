87a3968ea68265f9dc9bdbf32b88c609abb373cf
Eduardo Gallego garcia <egallego@c3r6s5.42madrid.com>
2019-12-08 15:34:12 +0100

    v.1.0 ft_memcmp.c ok ready ready

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/08 13:13:27 by egallego          #+#    #+#             */
+/*   Updated: 2019/12/08 15:09:37 by egallego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int			ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*string1;
+	unsigned char	*string2;
+
+	i = 0;
+	string1 = (unsigned char*)s1;
+	string2 = (unsigned char*)s2;
+	while (i < n)
+	{
+		if (string1[i] != string2[i])
+			return ((unsigned char)string1[i] - (unsigned char)string2[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: egallego <egallego@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 21:36:20 by egallego          #+#    #+#             */
-/*   Updated: 2019/12/06 18:52:06 by egallego         ###   ########.fr       */
+/*   Updated: 2019/12/08 13:29:19 by egallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
